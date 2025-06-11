a96973ab18598c5aa56041efb3b2191905b38a8f
Arturo Aponte Bonilla <arturoapontebonilla117@gmail.com>
2025-06-11 14:33:46 -0400

    Casi todo listo para ganar la base de datos

---
+++ WebApplicationViajeCero/Controllers/InstitutionsController.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplicationViajeCero.Models;
+using WebApplicationViajeCero.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApiViejaCero.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InstitutionsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public InstitutionsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        //GET api/Institutions
+
+        [HttpGet]
+
+        public async Task<ActionResult<IEnumerable<Institution>>> GetInstitution()
+        {
+            return await _context.Institutions.ToListAsync();
+
+        }
+
+        //GET api/Institutions/5
+
+        [HttpGet("{id}")]
+
+        public async Task<ActionResult<Institution>> GetInstitution(int id)
+        {
+            var institutions = await _context.Institutions.FindAsync();
+
+            if (institutions == null)
+            {
+                return NotFound();
+            }
+
+            return institutions;
+        }
+
+        //PUT api/Institutions
+
+        [HttpPut("{id}")]
+
+        public async Task<IActionResult> PutInstitution(int id, Institution institution)
+        {
+            if (id != institution.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(institution).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProvinceExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        //POST api/Institutions
+
+        [HttpPost]
+
+        public async Task<ActionResult<Institution>> PostInstitution(Institution institution)
+        {
+            _context.Institutions.Add(institution);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetInstitution", new {id = institution.Id}, institution);
+        }
+
+        //DELETE api/Institutions/5
+
+        [HttpDelete("{id}")]
+
+        public async Task<IActionResult> DeleteInstitution(int id)
+        {
+            var institution = await _context.Institutions.FindAsync(id);
+
+            if (institution == null)
+            {
+                return NotFound();
+            }
+
+            _context.Institutions.Remove(institution);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ProvinceExists(int id)
+        {
+            return _context.Provinces.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ WebApplicationViajeCero/Controllers/RolesController.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplicationViajeCero.Context;
+using WebApplicationViajeCero.Models;
+
+namespace WebApiViejaCero.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RolesController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public RolesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        //GET api/Roles
+        [HttpGet]
+
+        public async Task<ActionResult<IEnumerable<Role>>> GetRole()
+        {
+            return await _context.Roles.ToArrayAsync();
+        }
+
+        //GET api/Roles/5
+        [HttpGet("{id}")]
+
+        public async Task<ActionResult<Role>> GetRole(int id)
+        {
+            var role = await _context.Roles.FindAsync(id);
+
+            if (role == null)
+            {
+                return NotFound();   
+            }
+
+            return role;
+        }
+
+        //Put api/Roles/5
+
+        [HttpPut("{id}")]
+
+        public async Task<ActionResult<Role>> PutRole(int id, Role role) 
+        {
+            if (id != role.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(role).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!RoleExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        //POST api/Roles
+        [HttpPost]
+
+        public async Task<ActionResult<Role>> PostRole(Role role)
+        {
+            _context.Roles.Add(role);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetRole", new {id = role.Id}, role);
+        }
+
+        //DELETE api/Roles/5
+
+        [HttpDelete]
+
+        public async Task<IActionResult> DeleteRole(int id)
+        {
+            var role = await _context.Roles.FindAsync();
+
+            if (role == null)
+            {
+                return NotFound();
+            }
+
+            _context.Roles.Remove(role);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public bool RoleExists(int id)
+        {
+            return _context.Roles.Any(x => x.Id == id);
+        }
+    }
+}
