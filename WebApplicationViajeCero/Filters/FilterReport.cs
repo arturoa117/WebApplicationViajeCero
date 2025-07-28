7dafbf33c616d15374e302ec8f433043dc119efc
Saray Gonzalez <saraygonzalez209@gmail.com>
2025-07-28 11:24:13 -0400

    add filter taking into account date creation, date update and sexes

---
+++ WebApplicationViajeCero/Filters/FilterReport.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using WebApplicationViajeCero.Models;
+using WebApplicationViajeCero.Validations;
+
+namespace WebApplicationViajeCero.Filters
+{
+    public class FilterReport
+    {
+        public DateTime? from { get; set; }
+        public DateTime? to { get; set; }
+
+        [ValidGenderArray(ErrorMessage = "Solo se permiten los valores 'f' o 'm', máximo dos.")]
+        public char[]? Gender { get; set; }
+    }
+}
---
+++ WebApplicationViajeCero/Validations/ValidGenderArrayAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplicationViajeCero.Validations
+{
+    public class ValidGenderArrayAttribute : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            var genderArray = value as char[];
+
+            if (genderArray == null || genderArray.Length == 0)
+                return ValidationResult.Success; // nullable or empty is OK
+
+            if (genderArray.Length > 2)
+                return new ValidationResult("Solo se permiten hasta dos caracteres.");
+
+            if (genderArray.Any(g => g != 'f' && g != 'm'))
+                return new ValidationResult("El sexo debe ser solo 'f' o 'm'.");
+
+            return ValidationResult.Success;
+        }
+    }
+
+}
